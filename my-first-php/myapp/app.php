56f5a98b7c3e0858c707293c71c5871d8dbe6a98
Sohag Hossain <sohagofficial124@gmail.com>
2022-04-15 19:54:08 +0600

    Add files via upload

---
+++ my-first-php/myapp/app.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types = 1);
+
+//getting the csv file from it's path
+
+function getTransactionFiles(string $dirPath): array
+{
+    $files = [];
+
+    foreach (scandir($dirPath) as $file) {
+
+        if (is_dir($file)) {
+
+            continue;
+        }
+
+        $files[] =$dirPath . $file;
+    }
+
+    return $files;
+}
+
+//getting data from this csv file 
+
+function getTransactions(string $fileName): array
+{
+    if (! file_exists($fileName)) {
+
+        trigger_error('File "'. $fileName . '" does not exist.', E_USER_ERROR);
+    }
+
+    $file = fopen($fileName, 'r');
+
+    fgetcsv($file);
+
+    $transactions = [];
+    
+    while (($transaction = fgetcsv($file)) !== false) {
+        
+        $transactions[] = extractTransaction($transaction);
+    }
+
+    return $transactions;
+}
+
+// read transaction type
+
+function extractTransaction(array $transactionRow): array
+{
+    [$date, $checkNumber, $description, $amount] = $transactionRow;
+
+    $amount = (float) str_replace(['$', ','], '', $amount);
+
+
+    return [
+        'date'        => $date,
+        'checkNumber' => $checkNumber,
+        'description' => $description,
+        'amount'      => $amount,
+    ];
+}
+
+// amount calculation 
+
+function calculateTotals(array $transactions): array
+{
+    $totals = ['netTotal' => 0, 'totalIncome' => 0, 'totalExpense' => 0];
+
+    foreach ($transactions as $transaction) {
+        $totals['netTotal'] += $transaction['amount'];
+
+        if ($transaction['amount'] >= 0) {
+            $totals['totalIncome'] += $transaction['amount'];
+        } else {
+            $totals['totalExpense'] += $transaction['amount'];
+        }
+    }
+
+    return $totals;
+}
